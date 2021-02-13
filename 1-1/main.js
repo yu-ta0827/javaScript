182792cfddf33b122933dd8d5bbca0350c4518dc
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2021-02-13 13:49:08 +0900

    Initial commit

---
+++ 1-1/main.js
@@ -0,0 +1,16 @@
+console.log("HELLO WORLD!");
+console.log(65536);
+console.log(10+5);
+console.log(10*5);
+console.log("hello"+"world");
+// aaaaaaaaa
+/*こんにちわ
+おはよう
+*/
+
+window.alert("helloworld");
+
+// 課題
+console.log("10*8");
+console.log(10+8);
+console.log("20割る３のあまりは"+20%3);
