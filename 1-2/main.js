c8cd336f3cef759e3218a084104c86b0ed2c555c
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2021-02-13 14:13:48 +0900

    js1-2

---
+++ 1-2/main.js
@@ -0,0 +1,26 @@
+// const name = "hoge";
+// let name = "hogehoge";
+// console.log(name);
+
+// let name1 = "hoge";
+// console.log(name1);
+// let name1 = "fuga";
+// console.log(name1);
+
+// let name = "池田";
+// console.log(name+"くん");
+// let number =10;
+// console.log(number * 3);
+
+// console.log(`${name}君は${number}月生まれです`);
+
+// let name1 = 'iwasaki';
+//  name1 = 'tanaka';
+// console.log(name1);
+
+// 課題
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
