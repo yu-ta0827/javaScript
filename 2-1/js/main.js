0089a1bf182216d69b02ba60e3ecf30ee515e5c2
yu-ta0827 <74187289+yu-ta0827@users.noreply.github.com>
2021-03-11 22:33:57 +0900

    js

---
+++ 2-1/js/main.js
@@ -0,0 +1,10 @@
+// let target = document.getElementById('target');
+// console.log(target);
+
+// function myfunc() {
+//     let change = document.getElementById('target');
+
+//     change.textContent = 'こんにちわ';
+// }
+
+let target = document.getElementById('box');
